6694197705badcd0902e1c23b4bd64a0c532d0f1
thomas-gribling <tgriblin@student.42perpignan.fr>
2024-09-25 16:58:56 +0200

    z_buffer management

--- srcs_bonus/z_buffer_1.c
+++ srcs_bonus/z_buffer_1.c
@@ -1,16 +1,80 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils_raycasting_3.c                               :+:      :+:    :+:   */
+/*   z_buffer_1.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tgriblin <tgriblin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 16:29:57 by tgriblin          #+#    #+#             */
-/*   Updated: 2024/09/25 16:44:02 by tgriblin         ###   ########.fr       */
+/*   Updated: 2024/09/25 16:58:21 by tgriblin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d_bonus.h"
+
+static void	calc_dist_z_buffer(t_game *g)
+{
+	t_sprite	*t1;
+	t_enemy		*t2;
+	int			i;
+
+	i = -1;
+	while (++i < g->z_buffer_size)
+	{
+		if (g->z_buffer[i].type == SPRITE)
+		{
+			t1 = (t_sprite *)g->z_buffer[i].ptr;
+			t1->dist = dist_to_tile(g, t1->x, t1->y);
+		}
+		if (g->z_buffer[i].type == ENEMY)
+		{
+			t2 = (t_enemy *)g->z_buffer[i].ptr;
+			t2->dist = dist_to_tile(g, t2->x, t2->y);
+		}
+	}
+}
+
+static double	get_dist_z_buffer(t_game *g, int i)
+{
+	t_sprite	*t1;
+	t_enemy		*t2;
+	
+	if (g->z_buffer[i].type == SPRITE)
+	{
+		t1 = (t_sprite *)g->z_buffer[i].ptr;
+		return (t1->dist);
+	}
+	if (g->z_buffer[i].type == ENEMY)
+	{
+		t2 = (t_enemy *)g->z_buffer[i].ptr;
+		return (t2->dist);
+	}
+	return (0.0);
+}
+
+void	sort_z_buffer(t_game *g)
+{
+	t_actor	tmp;
+	int		i;
+	int		j;
+	int		max;
+
+	calc_dist_z_buffer(g);
+	i = -1;
+	while (++i < g->z_buffer_size)
+	{
+		max = i;
+		j = i;
+		while (++j < g->z_buffer_size)
+		{
+			if (get_dist_z_buffer(g, j) > get_dist_z_buffer(g, max))
+				max = j;
+		}
+		tmp = g->z_buffer[i];
+		g->z_buffer[i] = g->z_buffer[max];
+		g->z_buffer[max] = tmp;
+	}
+}
 
 void	fill_z_buffer(t_game *g)
 {
