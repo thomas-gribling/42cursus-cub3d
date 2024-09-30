a42f13ac7f28528f67e3e68a07e0470863266f60
thomas-gribling <tgriblin@student.42perpignan.fr>
2024-09-30 15:47:47 +0200

    better AI

--- srcs_bonus/enemies_1.c
+++ srcs_bonus/enemies_1.c
@@ -6,11 +6,24 @@
 /*   By: tgriblin <tgriblin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 09:10:26 by tgriblin          #+#    #+#             */
-/*   Updated: 2024/09/26 10:05:10 by tgriblin         ###   ########.fr       */
+/*   Updated: 2024/09/30 15:45:49 by tgriblin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d_bonus.h"
+
+static int	check_moves_nextbots(t_game *g, t_enemy *e, double *p)
+{
+	if (e->type < NEXTBOT_1 || e->type > NEXTBOT_3)
+		return (0);
+	if (!is_collision(g->map->content[(int)e->y][(int)p[0]])
+		&& fabs(e->x - g->p->x) > 0.1)
+		e->x = p[0];
+	if (!is_collision(g->map->content[(int)p[1]][(int)e->x])
+		&& fabs(e->y - g->p->y) > 0.1)
+		e->y = p[1];
+	return (1);
+}
 
 static void	check_moves(t_game *g, t_enemy *e)
 {
@@ -32,6 +45,8 @@ static void	check_moves(t_game *g, t_enemy *e)
 		p[0] = 0;
 	if (p[1] < 0)
 		p[1] = 0;
+	if (check_moves_nextbots(g, e, p))
+		return ;
 	if (!is_collision(g->map->content[(int)e->y][(int)p[0]]))
 		e->x = p[0];
 	if (!is_collision(g->map->content[(int)p[1]][(int)e->x]))
@@ -48,9 +63,9 @@ void	update_enemies(t_game *g)
 		if (!g->enemies[i].is_dead)
 		{
 			if (rand() % 10 == 0)
-				g->enemies[i].dirx = -1 + 2 * (rand() % 2);
+				g->enemies[i].dirx = rand() % 3 - 1;
 			if (rand() % 10 == 0)
-				g->enemies[i].diry = -1 + 2 * (rand() % 2);
+				g->enemies[i].diry = rand() % 3 - 1;
 			check_moves(g, &g->enemies[i]);
 		}
 	}
