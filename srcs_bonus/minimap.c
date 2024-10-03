7a20be2957765c4467b7588dc062a1ba77f3af8b
thomas-gribling <tgriblin@student.42perpignan.fr>
2024-10-03 13:58:50 +0200

    removed bullies from minimap

--- srcs_bonus/minimap.c
+++ srcs_bonus/minimap.c
@@ -6,42 +6,26 @@
 /*   By: tgriblin <tgriblin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 08:43:24 by tgriblin          #+#    #+#             */
-/*   Updated: 2024/10/02 09:04:10 by tgriblin         ###   ########.fr       */
+/*   Updated: 2024/10/03 13:57:12 by tgriblin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d_bonus.h"
 
-static int	enemy_here(t_game *g, int x, int y)
-{
-	int	i;
-
-	i = -1;
-	while (++i < g->enemies_amt)
-		if ((int)g->enemies[i].x == x && (int)g->enemies[i].y == y
-			&& g->enemies[i].type == BULLY && !g->enemies[i].is_dead)
-			return (1);
-	return (0);
-}
-
 static void	draw_minimap_tile(t_game *g, t_cam *c, int x, int y)
 {
-	t_tex	tex[4];
+	t_tex	tex[3];
 
 	tex[0] = g->tex[TEX_GUI_MAPWALL];
-	tex[1] = g->tex[TEX_GUI_MAPBULLY];
-	tex[2] = g->tex[TEX_GUI_MAPEXIT];
-	tex[3] = g->tex[TEX_GUI_0];
+	tex[1] = g->tex[TEX_GUI_MAPEXIT];
+	tex[2] = g->tex[TEX_GUI_0];
 	if (is_collision(g->map->content[y][x])
 		&& g->map->content[y][x] != ' ')
 		tex_put(&c->buff, &tex[0], 10 * (x + 1),
-			tex[3].height + 10 * (y + 1));
-	if (enemy_here(g, x, y))
-		tex_put(&c->buff, &tex[1], 10 * (x + 1),
-			tex[3].height + 10 * (y + 1));
+			tex[2].height + 10 * (y + 1));
 	if (g->map->content[y][x] == 'E')
-		tex_put(&c->buff, &tex[2], 10 * (x + 1),
-			tex[3].height + 10 * (y + 1));
+		tex_put(&c->buff, &tex[1], 10 * (x + 1),
+			tex[2].height + 10 * (y + 1));
 }
 
 void	draw_minimap(t_game *g, t_cam *c)
